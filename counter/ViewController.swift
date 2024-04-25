28110ad0fce8082193a6fe15a591f95d8c9f6e51
IITU <nur0911@mail.ru>
2024-04-25 22:38:07 +0500

    3

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  counter
-//
-//  Created by Daniyal on 25.04.2024.
+
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var number: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
+        
     }
 
 
-}
+    @IBAction func click(_ sender: Any) {
+     
+        
+           if var currentNumber = Int(number.text ?? "0") {
+               // Добавляем к числу 1
+               currentNumber += 1
+            
+               number.text = "\(currentNumber)"
+               print("nice")
+           }
+       }
+
+        
+    }
+    
+   
+    
+
 
